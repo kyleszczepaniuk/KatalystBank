32d6f8944156009d0ff1f0f7f291f1bca54e76cb
kyleszczepaniuk <53094230+kyleszczepaniuk@users.noreply.github.com>
2019-07-19 13:17:23 -0400

    Add files via upload
    Adding file

---
+++ Customer.java
@@ -0,0 +1,42 @@
+package com.web.CustomerWebService;
+
+public class Customer {
+	
+	public int id;
+	public String name;
+	public double balance;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	Customer(){}
+	Customer(int id, double balance, String name)
+	{
+		this.id = id;
+		this.balance = balance;
+		this.name = name;
+	}
+	
+	
+	public void printCustomerDetails()
+	{
+		String space = "  ";
+		System.out.println(id+space+balance+space+name);
+	}
+}
